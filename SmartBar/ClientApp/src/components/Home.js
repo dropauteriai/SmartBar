5b4582e19f589ce297ca81b242c681dd76b9892d
liepa1 <liepaite02@gmail.com>
2023-01-11 14:36:52 +0200

    darbas

--- SmartBar/ClientApp/src/components/Home.js
+++ SmartBar/ClientApp/src/components/Home.js
@@ -1,26 +1,67 @@
 import React, { Component } from 'react';
+import { Container, Col } from 'react-bootstrap';
+import Cards from './Cards.js';
+import SideBar from './SideBar.js';
+import Row from 'react-bootstrap/Row';
 
 export class Home extends Component {
   static displayName = Home.name;
 
+  constructor(props) {
+    super(props);
+    this.state = { tables: [], loading: true };
+  }
+
+  componentDidMount() {
+    console.log("start did Mount");
+    this.populateTableData();
+    console.log("end did Mount");
+  }
+
+  renderTableList(tables)
+  {
+    return (<Row xs={1} md={2}>
+        {tables.map((index, table) => 
+          {
+            <Cards
+            name={table.name} />;
+          }
+        )}
+        </Row>)
+  }
   render() {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : this.renderTableList(this.state.tables);
+
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+      <Container fluid>
+        <Row>
+            <Col xs={2} id="sidebar-wrapper">      
+              <SideBar />
+            </Col>
+            <Col  xs={10} id="page-content-wrapper">
+                
+       {/* <Row xs={1} md={2}>
+        {["1", "2", "3", "4", "5"].map((index) => (
+          <Cards 
+          name={index+ " stalas"}
+          />
+        ))}
+        <Cards name = "Baras"/>
+        </Row> */}
+        {contents}
+        </Col> 
+        </Row>
+        </Container>
       </div>
     );
+  }
+
+  async populateTableData() {
+    const response = await fetch('Table');
+    const data = await response.json();
+    this.setState({ tables: data, loading: false });
   }
 }
---
+++ SmartBar/Controllers/TableController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartBar.Controllers.DTO;
+
+namespace SmartBar.Controllers
+{
+    public class TableController : Controller
+    {
+        public List<TableDTO> GetAll()
+        {
+            var tables = Enumerable.Range(1, 5).Select(index => new TableDTO(index, $"Stalas {index}", index)).ToList();
+            tables.Add(new TableDTO(100, "Baras", 100));
+            return tables;
+        }
+        // GET: TableController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TableController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TableController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TableController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TableController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TableController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TableController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TableController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
